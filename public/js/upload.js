64db178f13ae569015a0f8c163e9a22828a1462c
Gomin <teelak1113@gmail.com>
2020-01-18 13:43:52 +0700

    Login Register and Movie hit daily

--- public/js/upload.js
+++ public/js/upload.js
@@ -125,3 +125,13 @@ buttonToday: 'picker__button--today'
     
 })
 
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
